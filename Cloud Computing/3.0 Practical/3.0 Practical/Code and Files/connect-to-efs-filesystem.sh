8a93689cc2192b3efec40866aa69de0088d0bf16
frank <wsuan15@gmail.com>
2023-07-03 00:37:27 +0800

    delete

--- Cloud Computing/3.0 Practical/3.0 Practical/Code and Files/connect-to-efs-filesystem.sh
+++
@@ -1,3 +0,0 @@
-sudo yum -y install amazon-efs-utils
-sudo mkdir /mnt/efs
-sudo mount -t efs -o tls fs-04ea272fe34c4a64e:/ /mnt/efs
--- Cloud Computing/Code and Files/Code and Files/install-stress-util-ec2.sh
+++
@@ -1,4 +0,0 @@
-# Amazon Linux 2023
-
-# Install the stress utility
-sudo yum install stress -y
